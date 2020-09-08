94977762c3f718328864d0652eba2b210bc036c7
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2020-09-08 18:05:27 +0200

    feat: use an own configuration based on industry standards
     * making it easier to upgrade
     * separation of concerns (editor, style, quality)
     * dogfood
     * generate default editorconfig if not available

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+const config: any = {
+  extends: [
+    "react-app",
+    // use prettier recommended
+    "plugin:prettier/recommended",
+    // disable typescript formatting stuff
+    "prettier/@typescript-eslint",
+  ],
+  rules: {
+    "no-var": "error",
+    "prefer-const": "error",
+    "prefer-destructuring": "error",
+    "prefer-spread": "error",
+    "prefer-template": "error",
+    "prefer-arrow-callback": "error",
+    "arrow-body-style": "error",
+    "prefer-promise-reject-errors": "error",
+    "prefer-rest-params": "error",
+  },
+};
+
+export = config;
