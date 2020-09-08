07cf62685dddf82e7751a5682aed81a26ada1b65
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2020-09-08 16:29:40 +0200

    feat: use an own configuration based on industry standards
     * making it easier to upgrade
     * separation of concerns (editor, style, quality)

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 This is the basic eslint config used by Kingsquare BV.
 
+## Implementation
+
+This config has been based on the idea that different tools should have different responsibilities.
+
+ * All configuration related to the editor (end of line, indent style, indent size...) should be handled by EditorConfig
+ * Everything related to _code formatting_ should be handled by Prettier
+ * The rest (_code quality_) should be handled by ESLint 
+
+See for background 
+
+ * https://blog.theodo.com/2019/08/empower-your-dev-environment-with-eslint-prettier-and-editorconfig-with-no-conflicts/
+ * https://blog.theodo.com/2019/08/why-you-should-use-eslint-prettier-and-editorconfig-together/
+
+##
+
 This version extends the [`react-app`](https://github.com/facebook/create-react-app/blob/master/packages/eslint-config-react-app/README.md) standard
 
 To install and use do the following:
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 module.exports = {
-  "extends": ["react-app", "plugin:prettier/recommended"],
-  "plugins": [
-    "prettier"
+  'extends': [
+    'react-app',
+    // use prettier recommended
+    'plugin:prettier/recommended',
+    // disable typescript formatting stuff
+    'prettier/@typescript-eslint'
   ],
-  "rules": {
-    "prettier/prettier": "error",
-    "no-var": "error",
-    "prefer-const": "error",
-    "prefer-destructuring": "error",
-    "prefer-spread": "error",
-    "prefer-template": "error",
-    "prefer-arrow-callback": "error",
-    "arrow-body-style": "error",
-    "prefer-promise-reject-errors": "error",
-    "prefer-rest-params": "error"
+  'plugins': [
+    'prettier'
+  ],
+  'rules': {
+    'prettier/prettier': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-destructuring': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'prefer-arrow-callback': 'error',
+    'arrow-body-style': 'error',
+    'prefer-promise-reject-errors': 'error',
+    'prefer-rest-params': 'error'
   }
 };
