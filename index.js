1e5b192bac1ed511227b0f4c7fba44a85ba662c7
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2020-09-08 16:47:30 +0200

    feat: use an own configuration based on industry standards
     * making it easier to upgrade
     * separation of concerns (editor, style, quality)
     * dogfood

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 module.exports = {
-  'extends': [
-    'react-app',
+  extends: [
+    "react-app",
     // use prettier recommended
-    'plugin:prettier/recommended',
+    "plugin:prettier/recommended",
     // disable typescript formatting stuff
-    'prettier/@typescript-eslint'
+    "prettier/@typescript-eslint",
   ],
-  'plugins': [
-    'prettier'
-  ],
-  'rules': {
-    'prettier/prettier': 'error',
-    'no-var': 'error',
-    'prefer-const': 'error',
-    'prefer-destructuring': 'error',
-    'prefer-spread': 'error',
-    'prefer-template': 'error',
-    'prefer-arrow-callback': 'error',
-    'arrow-body-style': 'error',
-    'prefer-promise-reject-errors': 'error',
-    'prefer-rest-params': 'error'
-  }
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "no-var": "error",
+    "prefer-const": "error",
+    "prefer-destructuring": "error",
+    "prefer-spread": "error",
+    "prefer-template": "error",
+    "prefer-arrow-callback": "error",
+    "arrow-body-style": "error",
+    "prefer-promise-reject-errors": "error",
+    "prefer-rest-params": "error",
+  },
 };
